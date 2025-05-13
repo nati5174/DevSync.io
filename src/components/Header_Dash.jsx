22f27e2b150be37534bc53b7af57552d1b66448c
Nethanel Reta <nethanel.n4@gmail.com>
2025-05-13 03:24:12 -0400

    added header

--- src/components/Header_Dash.jsx
+++ src/components/Header_Dash.jsx
@@ -1,3 +1,23 @@
- const Header_Dash =  () => {
-    
- }
+import React from 'react';
+import '../styles/header.css'
+
+
+
+const Header_Dash = () => {
+  return (
+    <header>
+
+      <div className='header'>
+
+      <h1>
+        Hello Doc
+
+      </h1>
+
+      </div>
+      
+    </header>
+  );
+};
+
+export default Header_Dash;
